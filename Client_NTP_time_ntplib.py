447ef0d7aeb2b184113d5ae9a98ada15e03b984e
LMONTALBANO <85928315+LMontalbano@users.noreply.github.com>
2021-06-17 15:25:17 +0200

    Update Client_NTP_time_ntplib.py
    Ajout d'une demande de serveur NTP au lancement du fichier.

--- Client_NTP_time_ntplib.py
+++ Client_NTP_time_ntplib.py
@@ -1,22 +1,24 @@
 import ntplib
 import time
 
-def get_network_time():
+def get_network_time(x):
     #start = time.time()
     c = ntplib.NTPClient()
-    response = c.request('pool.ntp.org')
+    response = c.request(x)
     ts = response.tx_time
     return time.ctime(ts)
 
+      
+print('Please enter an NTP server: ')
+x = input()
+print(x)
 
-if __name__ == "__main__":
-    
-    # t en second car on l'utilise pour le time.sleep()
-    t = 2
-    # boucle à True pour faire une boucle infinie
-    boucle = True
-    
-    while boucle:
-        print(get_network_time())
-        # time.sleep(t) permet de freeze le programme pendant une période t sec
-        time.sleep(t)
+# t en second car on l'utilise pour le time.sleep()
+t = 2
+# boucle à True pour faire une boucle infinie
+boucle = True
+
+while boucle:
+    print(get_network_time(x))
+    # time.sleep(t) permet de freeze le programme pendant une période t sec
+    time.sleep(t)
