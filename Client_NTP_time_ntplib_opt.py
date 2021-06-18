386ad7c222362b2dade050c9a17d20f7eabf12f0
LMONTALBANO <85928315+LMontalbano@users.noreply.github.com>
2021-06-18 11:48:48 +0200

    Update Client_NTP_time_ntplib_opt.py
    Gestion des Exceptions + possibilité de renseigner un serveur NTP en argument.

--- Client_NTP_time_ntplib_opt.py
+++ Client_NTP_time_ntplib_opt.py
@@ -2,28 +2,66 @@ import ntplib
 import time
 import sys
 import getopt
+import requests
+import datetime
+import logging
+import socket
 
 
-def get_network_time(x='pool.ntp.org'):
+def get_network_time(server='pool.ntp.org'):
     #start = time.time()
     c = ntplib.NTPClient()
-    response = c.request(x)
+    response = c.request(server)
     ts = response.tx_time
     return time.ctime(ts)
 
 
 if __name__ == "__main__":
-    
-    x = sys.argv[1:][0]
-    print('récupération de: ')
-    print(x)
-    
+
+    if len(sys.argv[1:]) == 1:
+        server = sys.argv[1:][0]
+                
+    else:
+        print('Please enter an NTP server: ')
+        server = input()
+        
     # t en second car on l'utilise pour le time.sleep()
-    t = 2
+    t = 1
     # boucle à True pour faire une boucle infinie
     boucle = True
-
+    
+    print('Server: ' + server)
+    
+    
+    
+    
     while boucle:
-        print(get_network_time(x))
-        # time.sleep(t) permet de freeze le programme pendant une période t sec
-        time.sleep(t)
+        logger = logging.getLogger()
+        try:
+            print(get_network_time(server))
+            # time.sleep(t) permet de freeze le programme pendant une période t sec
+            time.sleep(t)
+            
+        except ntplib.NTPException:
+            print("Error NTPException")
+            
+            logging.basicConfig(filename="std.log", 
+                    format='%(asctime)s %(message)s', 
+                    filemode='w')
+            logger.error("Error NTPException")
+            
+        except socket.gaierror:
+            print("Failed address lookup")
+            print('Please enter an NTP server: ')
+            server = input()
+            
+            logging.basicConfig(filename="std.log", 
+                    format='%(asctime)s %(message)s', 
+                    filemode='w')
+            logger.warning("Failed address lookup")
+            
+               
+            
+            
+            
+            
