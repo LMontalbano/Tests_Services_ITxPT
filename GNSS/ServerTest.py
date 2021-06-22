aaf531ec7daedca86160eca1db9369dc6a96d539
LMONTALBANO <85928315+LMontalbano@users.noreply.github.com>
2021-06-22 17:29:42 +0200

    Update ServerTest.py

--- GNSS/ServerTest.py
+++ GNSS/ServerTest.py
@@ -1,2 +1,14 @@
 import socket
+import time
 
+command = 'test.xml'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("127.0.0.1", 54239))
+
+s.send(command)
+
+time.sleep(2)
+resp = s.recv(3000)
+
+print(resp)
