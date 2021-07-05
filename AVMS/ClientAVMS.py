0ec080b674e0226dc4cd2a5addda77970229f02f
LMONTALBANO <85928315+LMontalbano@users.noreply.github.com>
2021-07-05 16:46:14 +0200

    Update ClientAVMS.py

--- AVMS/ClientAVMS.py
+++ AVMS/ClientAVMS.py
@@ -1,23 +1,33 @@
 import requests
-import http.client
-
 
 
 ###################Client##################
 ADDRESS = "http://127.0.0.1"
 PORT = 8000
 
-with open('xml_RunMonitoring_Sub.xml', "rb") as file:
+with open('xml_RunMonitoring_Unsub.xml', "rb") as file:
     rxml = file.read(4096)
 
-connection = http.client.HTTPConnection(ADDRESS, PORT)
-print('##### connection #####')
-print(connection)
 
 print('\n' + '##### rxml #####')
 print(rxml.decode())
 
-r = requests.post(ADDRESS + "/avms/runmonitoring", data= rxml)
+headers = {
+        "Content-Length": "",
+        "Accept-Encoding": "identity",
+        "Host": "127.0.0.1:8000",
+        "Content-Type": "text/xml"
+    }
+
+r = requests.post(ADDRESS + ':' + PORT + "/avms/runmonitoring", data= rxml, headers= headers)
+
+
+print('###############################################################################')
+print(r.request.headers)
+print('###############################################################################')
+
+print(r.status_code) # doit répondre 200
+print(r.status_code == requests.codes.ok) # doit répondre True
 
 print('##### reponse #####')
 print(r.text)
