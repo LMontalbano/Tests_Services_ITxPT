a06892387dcbaee6fbbf924466842724d635a22d
LMONTALBANO <85928315+LMontalbano@users.noreply.github.com>
2021-07-06 16:44:41 +0200

    Update ServerAVMS.py

--- AVMS/ServerAVMS.py
+++ AVMS/ServerAVMS.py
@@ -1,18 +1,29 @@
-import http.server
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-PORT = 8000
-SERV_ADDRESS = ("", PORT)
-
-
-server = http.server.HTTPServer
-handler = http.server.CGIHTTPRequestHandler
-handler.cgi_directories = ["/"]
-
-print("Serveur actif sur le port : {}".format(PORT))
-
-httpd = server(SERV_ADDRESS, handler)
-httpd.serve_forever()
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        pass
+        #self.send_response(200)
+        #self.send_header("Content-type", "text/xml")
+        #self.end_headers()
 
 
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        print(post_data) # <-- Print post data
+        print("\n")
+        #self._set_headers()
 
-    
+
+def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+
+    print(f"Starting httpd server on {addr}:{port}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run(addr='127.0.0.1', port=8000)
