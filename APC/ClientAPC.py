61be712b0008f5edef4fe9942e54d0d3b583f8e5
LMONTALBANO <85928315+LMontalbano@users.noreply.github.com>
2021-07-07 17:31:08 +0200

    Create APC repertory

---
+++ APC/ClientAPC.py
@@ -0,0 +1,33 @@
+import requests
+
+# Initialisation de l'address et du port
+ADDRESS = "http://127.0.0.1"
+PORT = 9000
+
+### RunMonitoring ###
+# Récupération du contenue du fichier PassengerDoorCount_Sub.xml
+with open('Sub_Unsub/PassengerDoorCount_Sub.xml', "rb") as file:
+    rxml0 = file.read(4096)
+    
+### RunMonitoring ###
+# Récupération du contenue du fichier PassengerVehicleCount_Sub.xml
+with open('Sub_Unsub/PassengerVehicleCount_Sub.xml', "rb") as file:
+    rxml1 = file.read(4096)
+
+
+
+# Création d'un headers personnalisé
+headers = {
+        "Content-Length": "",
+        "Accept-Encoding": "identity",
+        "Host": "127.0.0.1:9000",
+        "Content-Type": "text/xml"
+    }
+
+### POST Requests ###
+
+# Request pour le module passengerdoorcount
+r0 = requests.post(ADDRESS + ':' + PORT + "/apc/passengerdoorcount", data= rxml0, headers= headers)
+
+# Request pour le module passengervehiclecount
+r1 = requests.post(ADDRESS + ':' + PORT + "/apc/passengervehiclecount", data= rxml1, headers= headers)
---
+++ APC/ServerAPC.py
@@ -0,0 +1,41 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+# Création 
+class Server(BaseHTTPRequestHandler):
+    
+    # Setup du headers
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/xml")
+        self.end_headers()
+
+    # Setup du do_GET
+    def do_GET(self):
+        # Récupération de la taille des données
+        content_length = int(self.headers['Content-Length'])
+        
+        # Récupération des données
+        get_data = self.rfile.read(content_length)
+        
+        # Affichage des données sur la console
+        print(get_data)
+        print("\n")
+        #self._set_headers()
+
+
+def run(server_class=HTTPServer, handler_class=Server, addr="localhost", port=9000):
+    """ Fonction qui permet de run le serveur
+        Paramètres: server_class: , handler_class: la class Server par défaut, addr: localhost par défaut, port: 8000 par défaut
+        Return: Ne retourne rien, permet de faire tourné le serveur"""
+    
+    # Initialisation du server_address via les paramètre addr et port de la fonction    
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+
+    print(f"Starting httpd server on {addr}:{port}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    
+    run(addr='127.0.0.1', port=9000)
