6427e71f7c84b53d6f5c488ea01ee02f31894838
LMONTALBANO <85928315+LMontalbano@users.noreply.github.com>
2021-07-07 12:37:30 +0200

    Update ServerAVMS.py
    add commentary

--- AVMS/ServerAVMS.py
+++ AVMS/ServerAVMS.py
@@ -1,23 +1,34 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class S(BaseHTTPRequestHandler):
+# Création 
+class Server(BaseHTTPRequestHandler):
+    
+    # Setup du headers
     def _set_headers(self):
-        pass
-        #self.send_response(200)
-        #self.send_header("Content-type", "text/xml")
-        #self.end_headers()
+        self.send_response(200)
+        self.send_header("Content-type", "text/xml")
+        self.end_headers()
 
-
+    # Setup du do_POST
     def do_POST(self):
-        # Doesn't do anything with posted data
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        print(post_data) # <-- Print post data
+        # Récupération de la taille des données
+        content_length = int(self.headers['Content-Length'])
+        
+        # Récupération des données
+        post_data = self.rfile.read(content_length)
+        
+        # Affichage des données sur la console
+        print(post_data)
         print("\n")
         #self._set_headers()
 
 
-def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
+def run(server_class=HTTPServer, handler_class=Server, addr="localhost", port=8000):
+    """ Fonction qui permet de run le serveur
+        Paramètres: server_class: , handler_class: la class Server par défaut, addr: localhost par défaut, port: 8000 par défaut
+        Return: Ne retourne rien, permet de faire tourné le serveur"""
+    
+    # Initialisation du server_address via les paramètre addr et port de la fonction    
     server_address = (addr, port)
     httpd = server_class(server_address, handler_class)
 
@@ -26,4 +37,5 @@ def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
 
 
 if __name__ == "__main__":
+    
     run(addr='127.0.0.1', port=8000)
