018317024bbc4d9195955668a62dcbcf4ce856ed
LMONTALBANO <85928315+LMontalbano@users.noreply.github.com>
2021-07-08 16:28:43 +0200

    Update ServerAVMS.py

--- AVMS/ServerAVMS.py
+++ AVMS/ServerAVMS.py
@@ -1,10 +1,11 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import xml.etree.ElementTree as ET
 
+lspr = ''
 
 # Création 
 class Server(BaseHTTPRequestHandler):
-    
+
     # Setup du headers
     def _set_headers(self):
         self.send_response(200)
@@ -23,17 +24,42 @@ class Server(BaseHTTPRequestHandler):
         #print(post_data)
         #print("\n")
         #self._set_headers()
-        
-        print("DriverID : " + driver_id(post_data))
-        
-        print("Destination : " + destination_name(post_data))
 
-        print("Nom de ligne : " + line_name(post_data))
+        tree = ET.ElementTree(ET.fromstring(post_data))
+        root = tree.getroot()
 
-        print("Dernier Arrêt : " + last_stop_point_ref(post_data))
 
-        print("Heure d'arrivée prévue : " + time_next_stop(post_data)[0] + "\n" + "Heure d'arrivée éstimé : " + time_next_stop(post_data)[1])
+        for tag in root.findall("."):
+            if tag.tag == "PlannedPatternDelivery":
+                print("DriverID : ")
+                print(driver_id(post_data))
+                print("\n")
         
+                print("Destination : ")
+                print(destination_name(post_data))
+                print("\n")
+
+                print("Nom de ligne : ")
+                print(line_name(post_data))
+                print("\n")
+
+            
+            if tag.tag == "VehicleMonitoringDelivery":
+                print("Dernier Arrêt : ")
+                lspr = last_stop_point_ref(post_data)
+                print(lspr)
+                print("\n")
+
+            if tag.tag == "JourneyMonitoringDelivery" and lspr != '':
+
+                #print(post_data)
+                res = time_next_stop(post_data, lspr)
+                print("Heure d'arrivée prévue : ")
+                print(res[0])
+                print("Heure d'arrivée éstimé : ")
+                print(res[1])
+        
+            print("############################################################################################################")
         
         
         
@@ -41,7 +67,7 @@ def driver_id(data):
     
     tree = ET.ElementTree(ET.fromstring(data))
     root = tree.getroot()
-    
+
     for tag in root.findall("."):
         if tag.tag == "PlannedPatternDelivery":
             
@@ -99,35 +125,56 @@ def last_stop_point_ref(data):
 
     for tag in root.findall("."):
         if tag.tag == "VehicleMonitoringDelivery":
-
-            if tag.find("VehicleMonitoring/PreviousCallRef") is None:
-                return "Error, 'PreviousCallRef' tag not exists"
+            if tag.findall("VehicleActivity/ProgressBetweenStops") is None:
+                return "Error, 'ProgressBetweenStops' tag not exists"
             else:
-                if tag.find("VehicleMonitoring/PreviousCallRef/StopPointRef") is None:
-                    return "Error, 'StopPointRef' tag not exists"
+                if tag.find("VehicleActivity/ProgressBetweenStops/PreviousCallRef") is None:
+                    return "Error, 'PreviousCallRef' tag not exists"
                 else:
-                    for elem in root.findall("./VehicleMonitoring/PreviousCallRef/StopPointRef"):
-                        if elem.text is None:
-                            return "Error, 'StopPointRef' tag is empty"
-                        else:
-                            return elem.text
+                    if tag.find("VehicleActivity/ProgressBetweenStops/PreviousCallRef/StopPointRef") is None:
+                        return "Error, 'StopPointRef' tag not exists"
+                    else:
+                        for elem in root.findall("./VehicleActivity/ProgressBetweenStops/PreviousCallRef/StopPointRef"):
+                            if elem.text is None:
+                                return "Error, 'StopPointRef' tag is empty"
+                            else:
+                                return elem.text
 
 
-def time_next_stop(data): ########## A revoir car là je me base sur l'heure d'arrivé du dernier arrêt et pas du prochain #########
+def time_next_stop(data, lspr):
 
-    lspr = last_stop_point_ref(data)
+    #lspr = last_stop_point_ref(data)
+    #print(lspr)
 
     tree = ET.ElementTree(ET.fromstring(data))
     root = tree.getroot()
     
     for tag in root.findall("."):
         if tag.tag == "JourneyMonitoringDelivery":
-            
+
+            position = 0
             for elem in root.findall("./MonitoredJourney/OnwardCalls/OnwardCall"):
-                if elem["StopPointRef"].text == lspr:
-                    return (elem["PlannedArrivalTime"], elem["ExpectedArrivalTime"])
+                print("passage ici")
+                print(elem[0].text)
+                # elem[0] correspond à la balise StopPointRef
+                if elem[0].text != lspr:
+                    print(position)
+                    position +=1
+                    print(position)
+                else:
+                    if elem[0].text == lspr:
+                        print("passage là")
+                        for elem in root.findall("./MonitoredJourney/OnwardCalls"):
+                            print("passage là aussi")
+                            # elem[2] correspond à la balise PlannedArrivalTime et elem[3] correspond à la balise ExpectedArrivalTime
+                            return (elem[position +1][2], elem[position +1][3])
+                    else:
+                        return "Error, lspr n'existe pas"
+
+                    
 
 
+                    
 
 
 def run(server_class=HTTPServer, handler_class=Server, addr="localhost", port=8000):
